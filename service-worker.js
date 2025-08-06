a1f09592acfd69388a20617844952e36278f7dc3
Aquib Ahmed <aquibahmed21@gmail.com>
2025-08-06 18:22:47 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,6 @@
-const CACHE_NAME = "kitecite";
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/webrtc/manifest.json",
-  "/webrtc/src/app.js",
-  "/webrtc/src/media.js",
-  "/webrtc/src/recording.js",
-  "/webrtc/src/room.js",
-  "/webrtc/src/signalling.js",
-  "/webrtc/src/style.css",
-  "/webrtc/src/channel.css",
-  "/webrtc/images/kitecite.svg",
-  "/webrtc/images/icon-192.png",
-  "/webrtc/images/icon-512.png"
-];
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return Promise.all(
-          urlsToCache.map(url => {
-            return fetch(url)
-              .then(response => {
-                if (!response.ok) {
-                  throw new Error('Failed to fetch ' + url);
-                }
-                return cache.put(url, response);
-              });
-          })
-        );
-      })
-  );
+  
+
 });
 
 self.addEventListener("fetch", (event) => {
